64d83f6e6480e813eceef1211658fa76f0aa2bf0
Miguel <santosmiguel25@gmail.com>
2015-11-24 01:17:21 +0000

    Library ported from Atollic to CooCox

---
+++ TLC5941/tlc5941.h
@@ -0,0 +1,51 @@
+/*
+ * tlc5941.h
+ *
+ *  Created on: Nov 17, 2015
+ *      Author: Miguel
+ */
+
+#ifndef TLC5941_H_
+#define TLC5941_H_
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include <stdint.h>
+
+class TLC5941 {
+public:
+	TLC5941();
+	void init();
+
+	uint8_t getXERR();
+	/* Sets dot correction value (from 0 to 63) for a certain channel */
+	void setDot(uint8_t channel, uint8_t dot_val);
+	/* Sets brightness value (from 0 to 4095) for a certain channel */
+	void setChannel(uint8_t channel, uint16_t brightness);
+	/* Updates the chip by outputting the GS data */
+	static void update();
+
+private:
+	enum TLCPINS {
+		MODE 	= GPIO_Pin_4, /* 0 = In Dot Correction Mode | 1 = In Grayscale Mode (Output) */
+		SIN 	= GPIO_Pin_7, /* Serial input to TLC5941 (Output) */
+		SCLK 	= GPIO_Pin_5, /* Serial clock (Output) */
+		XLAT 	= GPIO_Pin_6, /* Serial data latch into the registers (Output) */
+		BLANK 	= GPIO_Pin_3, /* Turns off LEDS and signals the start of the Grayscale cycle (Output) */
+		XERR 	= GPIO_Pin_2  /* XERR signals LED overheating (Input) */
+	};
+
+	/* Modes: */
+	enum MD {
+		MD_GS, /* Grayscale mode */
+		MD_DC  /* Dot Correction Mode */
+	};
+
+	/* Send dot correction data: */
+	inline void sendDot(void);
+	/* Send grayscale data: */
+	inline void sendGS(void);
+};
+
+#endif /* TLC5941_H_ */
---
+++ TLC5941/tlcrgb.cpp
@@ -0,0 +1,109 @@
+/*
+ * tlcrgb.c
+ *
+ *  Created on: Nov 18, 2015
+ *      Author: Miguel
+ */
+
+#include "tlcrgb.h"
+
+TLC5941 TLCRGB::tlc;
+
+void TLCRGB::init(int rgb_index) {
+	index = rgb_index;
+	rgb_off = index * 3;
+	tlc.init();
+}
+
+TLCRGB::TLCRGB(int rgb_index) { init(rgb_index); }
+TLCRGB::TLCRGB() {}
+
+void TLCRGB::updateColor() {
+	tlc.setChannel(rgb_off, (uint16_t)rgb.r);
+	tlc.setChannel(rgb_off + 1, (uint16_t)rgb.g);
+	tlc.setChannel(rgb_off + 2, (uint16_t)rgb.b);
+}
+
+void TLCRGB::updateColor(rgb_t rgb_struct) {
+	set(rgb_struct);
+	updateColor();
+}
+
+void TLCRGB::updateAll() {
+	updateColor();
+	TLC5941::update();
+}
+
+void TLCRGB::updateAll(rgb_t rgb_struct) {
+	updateColor(rgb_struct);
+	TLC5941::update();
+}
+
+void TLCRGB::set(rgb_t rgb_struct) {
+	rgb.r = rgb_struct.r;
+	rgb.g = rgb_struct.g;
+	rgb.b = rgb_struct.b;
+}
+
+void TLCRGB::set(uint16_t r, uint16_t g, uint16_t b) {
+	rgb.r = r;
+	rgb.g = g;
+	rgb.b = b;
+}
+
+void TLCRGB::setR(uint16_t r) {
+	rgb.r = r;
+}
+
+void TLCRGB::setG(uint16_t g) {
+	rgb.g = g;
+}
+
+void TLCRGB::setB(uint16_t b) {
+	rgb.b = b;
+}
+
+TLC5941 TLCRGB::getTLC5941() {
+	return tlc;
+}
+
+void TLCRGB::led_test() {
+	#define LED_COUNT 3
+	TLCRGB leds[LED_COUNT];
+
+	#define update_all_leds() for(int i = 0; i < LED_COUNT; i++) leds[i].updateColor();
+
+	for(int i = 0; i < LED_COUNT; i++)
+		leds[i].init(i);
+
+	for(;;) {
+		for(int i=0;i<LED_COUNT;i++) {
+			switch(rand() % 3) {
+			case 0:
+				if(rand()%5 > 1) {
+					INC(leds[i].rgb.r, rand() % 3, rand()%0xFF+0x7F, 0);
+				} else {
+					DEC(leds[i].rgb.r, rand() % 3, rand()%0xFF+0x7F, 0);
+				}
+				break;
+			case 1:
+				if(rand()%5 > 1) {
+					INC(leds[i].rgb.g, rand() % 3,rand()%0xFF+0x7F, 0);
+				} else {
+					DEC(leds[i].rgb.g, rand() % 3,rand()%0xFF+0x7F, 0);
+				}
+				break;
+			case 2:
+				if(rand()%5 > 1) {
+					INC(leds[i].rgb.b, rand() % 3, rand()%0xFF+0x7F, 0);
+				} else {
+					DEC(leds[i].rgb.b, rand() % 3, rand()%0xFF+0x7F, 0);
+				}
+				break;
+			}
+		}
+
+		update_all_leds();
+		TLC5941::update();
+	}
+}
